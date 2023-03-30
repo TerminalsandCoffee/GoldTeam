9ceae8ec67a83cefca191ef2bd42a2d0ee33f2d4
Rafael Martinez <rgmllc@yahoo.com>
2023-03-30 21:53:14 +0000

    "This is the basic portion of this weeks python project - EC2 name generator"

--- EC2NameGenerator.py
+++ EC2NameGenerator.py
@@ -1,69 +1,25 @@
-from random import choice
+import random 
 import string
-import sys
-
-def string_generator(size=6, string=string.ascii_letters + string.digits):
-    return ''.join(choice(string) for _ in range(size))
-
-print()
-print("WARNING: Only Accounting, FinOps, and Marketing are authorized to use")
-print()
-department = input("Enter Department: ")  
-    
-for _ in department:
-    
-    if department == "Marketing" or department.lower() == "marketing" :
-        break
-    elif department == "Accounting" or department.lower() == "accounting" :
-        break
-    elif department == "FinOps" or department.lower() == "finops" :
-        break
-    else:
-        print("Error: You can not use this generator, enter the name correctly.")
-        raise SystemExit
-        sys.exit()  
-
-number = int(input("Enter the number of EC2 instances required: "))
-
-if number < 0:
-    print("Please enter a positive number: ")
-
-elif number > 0:
-    print()
-
-print("##########################")
-print("   EC2 Instance Names  ")
-print("##########################")
-print()
-
-for _ in range(1, number + 1):
-    EC2_unique_name = department + "-" + string_generator()
-    print("Your EC2 Instance's unique name is: ", EC2_unique_name)
 
 
-
-#One way to improve the efficiency of the code would be to use a list of authorized departments instead of multiple if-elif statements. 
-#You can use the in keyword to check if the department entered by the user is in the list of authorized departments. 
-
-
-authorized_departments = ["Accounting", "FinOps", "Marketing"]
-
-department = input("Enter Department: ")
-
-if department.title() not in authorized_departments:
-    print("Error: You can not use this generator, enter the name correctly.")
-    raise SystemExit
-
-
-#Another improvement would be to use a while loop for validating the input for the number of instances until a positive number is entered.
-
-
+#number of EC2 instances they want names for
 while True:
     try:
-        number = int(input("Enter the number of EC2 instances required: "))
-        if number > 0:
-            break
-        else:
-            print("Please enter a positive number: ")
+        num_of_instances = int(input("Hello, please enter the number of EC2 instances you want names for: "))
+        break
     except ValueError:
-        print("Please enter a valid number: ")
+        print("Sorry, the input must be a number. Please try again.")
+
+# continue with the rest of your code using num_of_instances
+
+
+#input name of department
+dept_name = input("Enter the name of your department: ")
+
+for i in range(num_of_instances):
+#string of random numbers + letters
+  random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+
+#concatenate
+  unique_name = dept_name + "-" + random_string
+  print(unique_name)
