f351e29c1032b538e1d26e3ca576dd2dad3e1594
Rafael Martinez <rgmllc@yahoo.com>
2023-04-10 22:54:49 +0000

    dis da one!

--- StopEC2.py
+++ StopEC2.py
@@ -4,23 +4,21 @@ This is a Python script using the Boto3 library that will stop three EC2 instanc
 
 import boto3
 
-# Specify the region where your instances are running
-region = 'us-east-1'
+# Create an EC2 client
+ec2 = boto3.client('ec2')
 
-# Specify the IDs of the instances you want to stop
-instance_ids = ['i-xxx', 'i-xxx', 'i-xxx']
+# Get all running instances
+instances = ec2.describe_instances(
+    Filters=[
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ]
+)
 
-# Create an EC2 client object
-ec2 = boto3.client('ec2', region_name=region)
-
-# Stop the instances
-ec2.stop_instances(InstanceIds=instance_ids)
-
-print('Stopped instances: ' + ', '.join(instance_ids))
-
-
-"""
-Replace the instance IDs
-Note that you will need to have valid AWS credentials configured for the script to be able to access your instances.
-
-"""
+# Stop each instance
+for reservation in instances['Reservations']:
+    for instance in reservation['Instances']:
+        print(f"Stopping instance {instance['InstanceId']}")
+        ec2.stop_instances(InstanceIds=[instance['InstanceId']])
