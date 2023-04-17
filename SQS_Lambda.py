2130f5b36767effe9dd8172b19660369360d21c4
Rafael Martinez <rgmllc@yahoo.com>
2023-04-17 21:52:03 +0000

    lambda code is now more effcient

--- SQS_Lambda.py
+++ SQS_Lambda.py
@@ -2,20 +2,18 @@ import json
 import boto3
 from datetime import datetime
 
+# Create SQS client and get queue URL
+sqs = boto3.client('sqs', region_name='us-east-1')
+queue_url = 'https://sqs.us-east-1.amazonaws.com/397758027793/OrderTracker'
+
 def lambda_handler(event, context):
+    # Get the current time as a formatted string
+    date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     
-    current_time = datetime.now()  # gets the current time 
+    # Send the message to the queue
+    response = sqs.send_message(QueueUrl=queue_url, MessageBody=date_time)
     
-    date_time = current_time.strftime("%m/%d/%Y, %H:%M:%S") #coverts current time into string and unicode
-    
-    sqs = boto3.client('sqs') # gets the service client 
-    
-    response = sqs.send_message(
-    
-    QueueUrl='https://sqs.us-east-1.amazonaws.com/397758027793/OrderTracker',  # adding URL of the SQS queue
-    
-    MessageBody= date_time) # body of the message 
-    
+    # Return a response
     return {
         'statusCode': 200,
         'body': json.dumps(date_time)
