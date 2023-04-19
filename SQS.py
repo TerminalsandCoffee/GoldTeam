f76a74c13fae10b404cf04f63aaec2e0fd650f4e
Rafael Martinez <rgmllc@yahoo.com>
2023-04-19 20:54:05 +0000

    cleaned up the code

--- SQS.py
+++ SQS.py
@@ -8,35 +8,3 @@ queue = sqs.create_queue(QueueName='OrderTracker')
 
 #URL that will be needed for our next code. 
 print(queue.url)
-
-
-
-
-"""
-import boto3
-import datetime
-
-# Get the service resource
-sqs = boto3.resource('sqs', region_name='us-east-1')
-
-# Define name
-queue_name = 'OrderTracker'
-
-def lambda_handler(event, context):
-    #get the URL
-    queue_url = sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
-    
-    # Get the current time as a string
-    current_time = str(datetime.datetime.now())
-    
-    # Send the message to the queue
-    response = sqs.send_message(
-        QueueUrl=queue_url,
-        MessageBody=current_time
-    )
-    
-    # Print the response to the log
-    print(response)
-    
-
-"""
